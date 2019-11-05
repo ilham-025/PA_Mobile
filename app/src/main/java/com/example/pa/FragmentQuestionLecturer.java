10b1617bd26790ee03e08435e9caead608fe62c0
ilham <ilhamdotcom81@gmail.com>
2019-11-05 12:40:21 +0800

    add layout

--- app/src/main/java/com/example/pa/FragmentQuestionLecturer.java
+++ app/src/main/java/com/example/pa/FragmentQuestionLecturer.java
@@ -1,24 +1,36 @@
 package com.example.pa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentQuestionLecturer extends Fragment {
+    private FloatingActionButton btnAdd;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_question_lecturer, container, false);
+        btnAdd = view.findViewById(R.id.btn_add_question);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent move = new Intent(getContext(), add_question.class);
+                startActivity(move);
+            }
+        });
     }
 }
