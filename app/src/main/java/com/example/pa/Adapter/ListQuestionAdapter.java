a1bfbc9720460fd2cf64ad279ca5591b52d30655
ilham-025 <ilhamdotcom81@gmail.com>
2019-12-04 14:06:09 +0800

    lol lagi

---
+++ app/src/main/java/com/example/pa/Adapter/ListQuestionAdapter.java
@@ -0,0 +1,62 @@
+package com.example.pa.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pa.Model.Problem;
+import com.example.pa.R;
+
+import java.util.ArrayList;
+
+public class ListQuestionAdapter extends RecyclerView.Adapter<ListQuestionAdapter.ViewHolder> {
+
+    private ArrayList<Problem> problems;
+    private OnQuestionListListener onQuestionListListener;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_question_lecturer, parent, false);
+        return new ViewHolder(view, onQuestionListListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Problem problem = problems.get(position);
+        holder.judul.setText(problem.getTitle());
+        holder.tempo.setText(problem.getStartTime()+" "+problem.getStartDate()+"-"+problem.getEndTime()+" "+problem.getEndDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return problems.size();
+    }
+
+    public void setListProblem(ArrayList<Problem> problems){
+        this.problems = problems;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView judul, tempo;
+        public ViewHolder(@NonNull View itemView, OnQuestionListListener onQuestionListListener) {
+            super(itemView);
+            judul = itemView.findViewById(R.id.tv_judul_soal);
+            tempo = itemView.findViewById(R.id.tv_tempo);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onQuestionListListener.onClick(getAdapterPosition(), problems.get(getAdapterPosition()), true);
+        }
+    }
+    public interface OnQuestionListListener{
+        void onClick(int position, Problem problem, boolean isEdit);
+
+    }
+}
