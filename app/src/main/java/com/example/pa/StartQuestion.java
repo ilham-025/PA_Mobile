d292bf7b920bb8584c81e0d3bd06334f2bf12e3c
ilham-025 <ilhamdotcom81@gmail.com>
2019-12-10 10:22:12 +0800

    lol lagi

---
+++ app/src/main/java/com/example/pa/StartQuestion.java
@@ -0,0 +1,14 @@
+package com.example.pa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_question);
+    }
+}
