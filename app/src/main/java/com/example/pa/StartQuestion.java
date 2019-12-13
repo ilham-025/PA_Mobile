5e746c5ee4d620968acf7023909124ffebb6e633
ilham-025 <ilhamdotcom81@gmail.com>
2019-12-13 20:17:00 +0800

    start question

--- app/src/main/java/com/example/pa/StartQuestion.java
+++ app/src/main/java/com/example/pa/StartQuestion.java
@@ -3,6 +3,10 @@ package com.example.pa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class StartQuestion extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class StartQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_question);
+
+        LinearLayout lySoal = findViewById(R.id.ly_soal);
+
+        int item = 1;
+        TextView txt = new TextView(StartQuestion.this);
+        txt.setText(item+". ");
+
+        EditText edt = new EditText(StartQuestion.this);
+        edt.setWidth(lySoal.getWidth());
+        edt.setHint("Pertanyaan");
+        edt.setId(View.generateViewId());
+        EditText edt2 = new EditText(StartQuestion.this);
+        edt2.setHint("Jawaban");
+        edt2.setWidth(lySoal.getWidth());
+        edt2.setId(View.generateViewId());
+
+        LinearLayout lay = new LinearLayout(StartQuestion.this);
+        lay.setOrientation(LinearLayout.VERTICAL);
+        lay.addView(edt);
+        lay.addView(edt2);
+
+        LinearLayout lay2 = new LinearLayout(StartQuestion.this);
+        lay2.setOrientation(LinearLayout.HORIZONTAL);
+        lay2.addView(txt);
+        lay2.addView(lay);
+        lySoal.addView(lay2);
+        item++;
     }
 }
